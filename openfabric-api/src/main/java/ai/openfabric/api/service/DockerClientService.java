08aa8e006c8908549362cf38830408670b49f342
natnael-meresa <natnaelmeresa6@gmail.com>
2023-04-06 08:52:51 +0300

    fix config issue

--- openfabric-api/src/main/java/ai/openfabric/api/service/DockerClientService.java
+++ openfabric-api/src/main/java/ai/openfabric/api/service/DockerClientService.java
@@ -1,11 +1,13 @@
 package ai.openfabric.api.service;
 
+import ai.openfabric.api.config.DockerConfig;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,12 @@ import org.springframework.stereotype.Service;
 public class DockerClientService {
 
     @Value("${docker.host}")
-    private String dockerHost;
-
+    private String dockerHost = "tcp://localhost:2375";
     @Value("${docker.tls-verify}")
-    private Boolean dockerTlsVerify;
-
+    private Boolean dockerTlsVerify = false;
     @Value("${docker.cert-path}")
-    private String dockerCertPath;
+    private String dockerCertPath = "/home/user/.docker";
+
 
     public DockerClient InstantiatDockerClient () {
 
